9576d64198afc184b1871d3d92ca019239e0679b
NilliVanilli <nielsen.hendrik@googlemail.com>
2025-10-08 23:19:55 +0200

    bump version 1.1.6

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 // Names (base = your fork, non-base = upstream)
 const (
 	AppName     = "EPGo-Docker" // your Docker fork
-	Version     = "v1.1.5"
+	Version     = "v1.1.6"
 	BaseName    = "EPGo"        // upstream app
 	BaseVersion = "v3.2.1"
 )
