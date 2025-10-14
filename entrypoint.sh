6382fb0ba8a2bcfce90846ae706472e3a30fe420
NilliVanilli <nielsen.hendrik@googlemail.com>
2025-10-14 13:54:03 +0200

    fix-typo

--- entrypoint.sh
+++ entrypoint.sh
@@ -113,36 +113,4 @@ else
   echo "Error: No execution mode defined."
   echo "Please set either the CRON_SCHEDULE or RUN_ONCE environment variable."
   exit 1
-fiEPGO_CMD="/sbin/su-exec ${PUID}:${PGID} sh -c 'cd /app && /usr/bin/epgo -config /app/config.yaml'"
-
-# Case 1: RUN_ONCE is set to "true"
-if [ "${RUN_ONCE}" = "true" ]; then
-    echo "RUN_ONCE is true. Running epgo command once..."
-    eval $EPGO_CMD
-    echo "Task complete. Exiting."
-    exit 0
-
-# Case 2: CRON_SCHEDULE is set
-elif [ -n "${CRON_SCHEDULE}" ]; then
-    echo "CRON_SCHEDULE is set. Configuring cron job..."
-    
-    CLEAN_SCHEDULE=$(echo "${CRON_SCHEDULE}" | sed -e 's/^"//' -e 's/"$//')
-
-    echo "${CLEAN_SCHEDULE} ${EPGO_CMD} >> /proc/1/fd/1 2>> /proc/1/fd/2" > /etc/crontabs/root
-    echo "" >> /etc/crontabs/root
-
-    NEXT_RUN_TIME=$(/usr/local/bin/nextrun "${CLEAN_SCHEDULE}")
-    echo "Next execution scheduled for: ${NEXT_RUN_TIME}"
-    
-    echo "Starting cron daemon in the background."
-    crond -b -l 8
-
-    echo "Container is running in cron mode."
-    tail -f /dev/null
-
-# Case 3: Neither variable is set
-else
-    echo "Error: No execution mode defined."
-    echo "Please set either the CRON_SCHEDULE or RUN_ONCE environment variable."
-    exit 1
 fi
